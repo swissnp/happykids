420a5fa71b050555775a9d9d729c900bdd49ab42
swiss0404 <swiss0404@gmail.com>
2023-04-12 21:15:43 +0700

    fix: caching

--- next.config.js
+++ next.config.js
@@ -6,14 +6,7 @@
 
 const config = {
   reactStrictMode: true,
-  images: { remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'skillkamp-api.com',
-      port: '',
-      pathname: '/*',
-    },
-  ], },
+  images: { domains: ["skillkamp-api.com"] },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
@@ -33,24 +26,28 @@ const withPWA = require("next-pwa")({
   // disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     {
-      urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
-      handler: "CacheFirst",
+      // MUST be the same as "start_url" in manifest.json
+      urlPattern: "/",
+      // use NetworkFirst or NetworkOnly if you redirect un-authenticated user to login page
+      // use StaleWhileRevalidate if you want to prompt user to reload when new version available
+      handler: "NetworkFirst",
       options: {
-        cacheName: "google-fonts-webfonts",
+        // don't change cache name
+        cacheName: "start-url",
         expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+          maxEntries: 1,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
         },
       },
     },
     {
-      urlPattern: /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
-      handler: "StaleWhileRevalidate",
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: "CacheFirst",
       options: {
-        cacheName: "google-fonts-stylesheets",
+        cacheName: "google-fonts",
         expiration: {
           maxEntries: 4,
-          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
         },
       },
     },
@@ -72,41 +69,6 @@ const withPWA = require("next-pwa")({
         cacheName: "static-image-assets",
         expiration: {
           maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\/_next\/image\?url=.+$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "next-image",
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:mp3|wav|ogg)$/i,
-      handler: "CacheFirst",
-      options: {
-        rangeRequests: true,
-        cacheName: "static-audio-assets",
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:mp4)$/i,
-      handler: "CacheFirst",
-      options: {
-        rangeRequests: true,
-        cacheName: "static-video-assets",
-        expiration: {
-          maxEntries: 32,
           maxAgeSeconds: 24 * 60 * 60, // 24 hours
         },
       },
@@ -134,17 +96,6 @@ const withPWA = require("next-pwa")({
       },
     },
     {
-      urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "next-data",
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
       urlPattern: /\.(?:json|xml|csv)$/i,
       handler: "StaleWhileRevalidate",
       options: {
@@ -156,17 +107,7 @@ const withPWA = require("next-pwa")({
       },
     },
     {
-      urlPattern: ({ url }) => {
-        const isSameOrigin = self.origin === url.origin;
-        if (!isSameOrigin) return false;
-        const pathname = url.pathname;
-        // Exclude /api/auth/callback/* to fix OAuth workflow in Safari without impact other environment
-        // Above route is default for next-auth, you may need to change it if your OAuth workflow has a different callback route
-        // Issue: https://github.com/shadowwalker/next-pwa/issues/131#issuecomment-821894809
-        if (pathname.startsWith("/api/auth/")) return false;
-        if (pathname.startsWith("/api/")) return true;
-        return false;
-      },
+      urlPattern: /\/api\/.*$/i,
       handler: "NetworkFirst",
       method: "GET",
       options: {
@@ -179,34 +120,59 @@ const withPWA = require("next-pwa")({
       },
     },
     {
-      urlPattern: ({ url }) => {
-        const isSameOrigin = self.origin === url.origin;
-        if (!isSameOrigin) return true;
-        const pathname = url.pathname;
-        if (pathname.startsWith("/api/")) return false;
-        return true;
+      urlPattern: /\/_next\/image\?url=.*$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "next-image",
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
       },
+    },
+    {
+      urlPattern: /^https:\/\/raw\.githubusercontent\.com\/marcofranssen\/.*/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "github-code-samples",
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/www\.google-analytics\.com\/.*/i,
+      handler: "NetworkOnly",
+    },
+    {
+      urlPattern: /^https:\/\/pagead2\.googlesyndication\.com\/.*/i,
+      handler: "NetworkOnly",
+    },
+    {
+      urlPattern: /^https:\/\/google.com\/ads\/.*/i,
+      handler: "NetworkOnly",
+    },
+    {
+      urlPattern: /^https:\/\/googleads\.g\.doubleclick\.net\/pagead\/ads.*/i,
+      handler: "NetworkOnly",
+    },
+    {
+      urlPattern: /^https:\/\/partner\.googleadservice\.com\/.*/i,
+      handler: "NetworkOnly",
+    },
+    {
+      urlPattern: /^https:\/\/adservice\.google\..*/i,
+      handler: "NetworkOnly",
+    },
+    {
+      urlPattern: /.*/i,
       handler: "NetworkFirst",
       options: {
         cacheName: "others",
         expiration: {
           maxEntries: 32,
           maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-        networkTimeoutSeconds: 10,
-      },
-    },
-    {
-      urlPattern: ({ url }) => {
-        const isSameOrigin = self.origin === url.origin;
-        return !isSameOrigin;
-      },
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "cross-origin",
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 60 * 60, // 1 hour
         },
         networkTimeoutSeconds: 10,
       },
