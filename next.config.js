98a051115c609e53c407fa974750446474a83ab5
swiss0404 <swiss0404@gmail.com>
2023-04-12 21:26:10 +0700

    test

--- next.config.js
+++ next.config.js
@@ -6,7 +6,14 @@
 
 const config = {
   reactStrictMode: true,
-  images: { domains: ["skillkamp-api.com"] },
+  images: { remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'skillkamp-api.com',
+      port: '',
+      pathname: '/static/**',
+    },
+  ], },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
@@ -24,160 +31,6 @@ const config = {
 const withPWA = require("next-pwa")({
   dest: "public",
   // disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      // MUST be the same as "start_url" in manifest.json
-      urlPattern: "/",
-      // use NetworkFirst or NetworkOnly if you redirect un-authenticated user to login page
-      // use StaleWhileRevalidate if you want to prompt user to reload when new version available
-      handler: "NetworkFirst",
-      options: {
-        // don't change cache name
-        cacheName: "start-url",
-        expiration: {
-          maxEntries: 1,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "google-fonts",
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-font-assets",
-        expiration: {
-          maxEntries: 4,
-          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-image-assets",
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:js)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-js-assets",
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:css|less)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-style-assets",
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:json|xml|csv)$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "static-data-assets",
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\/api\/.*$/i,
-      handler: "NetworkFirst",
-      method: "GET",
-      options: {
-        cacheName: "apis",
-        expiration: {
-          maxEntries: 16,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-        networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
-      },
-    },
-    {
-      urlPattern: /\/_next\/image\?url=.*$/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "next-image",
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/raw\.githubusercontent\.com\/marcofranssen\/.*/i,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "github-code-samples",
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/www\.google-analytics\.com\/.*/i,
-      handler: "NetworkOnly",
-    },
-    {
-      urlPattern: /^https:\/\/pagead2\.googlesyndication\.com\/.*/i,
-      handler: "NetworkOnly",
-    },
-    {
-      urlPattern: /^https:\/\/google.com\/ads\/.*/i,
-      handler: "NetworkOnly",
-    },
-    {
-      urlPattern: /^https:\/\/googleads\.g\.doubleclick\.net\/pagead\/ads.*/i,
-      handler: "NetworkOnly",
-    },
-    {
-      urlPattern: /^https:\/\/partner\.googleadservice\.com\/.*/i,
-      handler: "NetworkOnly",
-    },
-    {
-      urlPattern: /^https:\/\/adservice\.google\..*/i,
-      handler: "NetworkOnly",
-    },
-    {
-      urlPattern: /.*/i,
-      handler: "NetworkFirst",
-      options: {
-        cacheName: "others",
-        expiration: {
-          maxEntries: 32,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-        networkTimeoutSeconds: 10,
-      },
-    },
-  ],
 });
 
 module.exports = withPWA({
